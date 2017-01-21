f4d2a82cf70d68bdb07c1ccb415fbb2db139a1ad
anshulvaid <anshulvaid@users.noreply.github.com>
2017-01-21 07:43:05 -0800

    Completed PartA of assignment 1

--- project.py
+++ project.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import re
+import sys
 
 class Token:
 
@@ -16,4 +17,29 @@ def tokenize(TextFilePath):
 				tokenList.append(token)
 	return tokenList
 
+def computeWordFrequencies(TokenList):
+	freq = {}
+	for token in TokenList:
+		lower = token.val.lower()
+		if lower in freq:
+			freq[lower] += 1
+		else:
+			freq[lower] = 1
+	return freq
 
+
+def printF(Frequencies):
+	for k,v in sorted(Frequencies.items(), key=lambda t:t[1], reverse=True):
+		print("{0} -> {1}".format(k,v))
+
+def main(argv):
+	if len(argv) < 2:
+		print("Please enter the file name.")
+	else:
+		tokens = tokenize(argv[1])
+		freq = computeWordFrequencies(tokens)
+		printF(freq)
+
+if __name__ == "__main__":
+	main(sys.argv)
+	
