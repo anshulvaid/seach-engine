6253361d9f93d00bc1819c83606ddc17815ca408
anshulvaid <anshulvaid@users.noreply.github.com>
2017-01-22 19:08:19 -0800

    Printing results sorted by count, word; Splitting on all punctuation marks

--- TextProcessing/PartA_1.py
+++ TextProcessing/PartA_1.py
@@ -2,34 +2,35 @@
 import re
 import sys
 
-class Token:
+# class Token:
 
-	def __init__(self, value):
-		self.val = value
+# 	def __init__(self, value):
+# 		self.val = value
 
 def tokenize(TextFilePath):
 	tokenList = []
 	with open(TextFilePath) as fi:
 		for line in fi:
-			words = filter(None,re.findall("[\w'-]+", line))
-			for word in words:
-				token = Token(word)
-				tokenList.append(token)
+			words = filter(None,re.split("\W+|_", line))
+			tokenList += words
+			# for word in words:
+			# 	token = Token(word)
+			# 	tokenList.append(token)
 	return tokenList
 
 def computeWordFrequencies(TokenList):
 	freq = {}
 	for token in TokenList:
-		lower = token.val.lower()
+		lower = token.lower()
 		if lower in freq:
 			freq[lower] += 1
 		else:
-			freq[lower] = 1
+		 	freq[lower] = 1
 	return freq
 
 
 def printF(Frequencies):
-	for k,v in sorted(Frequencies.items(), key=lambda t:t[1], reverse=True):
+	for k,v in sorted(Frequencies.items(), key=lambda t:(t[1],t[0]), reverse=True):
 		print("{0} -> {1}".format(k,v))
 
 def main(argv):
